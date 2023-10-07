25646f62f1f31baba82dca5d8a29d8f5ca2ed539
diegoreiss <diegoportal.reis@outlook.com>
2023-10-07 20:34:35 -0300

    start spring

---
+++ src/test/java/com/springboot/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
